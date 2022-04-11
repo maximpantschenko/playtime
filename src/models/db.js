a5d5dd4643495e0254bb8a611e84a2e7f40bb9c3
Maxim Pantschenko <pantschenkomaxim@gmail.com>
2022-04-11 17:53:23 +0200

    update lab 01

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,6 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { playlistMemStore } from "./mem/playlist-mem-store.js";
+import { trackMemStore } from "./mem/track-mem-store.js";
 
 export const db = {
   userStore: null,
@@ -8,5 +9,12 @@ export const db = {
   init() {
     this.userStore = userMemStore;
     this.playlistStore = playlistMemStore;
+    this.trackStore = strackMemStore;
+  },
+
+  async getPlaylistById(id){
+    const list = playlists.find((playlist) => playlist._id === id);
+    list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
+    return list;
   },
 };
