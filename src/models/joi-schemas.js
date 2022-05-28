a2ca736716374966af4f59ecad4fc97f8f4f160b
maximpantschenko <50883771+maximpantschenko@users.noreply.github.com>
2022-05-28 13:48:10 +0200

    lab10a

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,5 +1,12 @@
 import Joi from "joi";
 
+export const JwtAuth = Joi.object()
+  .keys({
+    success: Joi.boolean().example("true").required(),
+    token: Joi.string().example("eyJhbGciOiJND.g5YmJisIjoiaGYwNTNjAOhE.gCWGmY5-YigQw0DCBo").required(),
+  })
+  .label("JwtAuth");
+
 export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
 
 export const UserCredentialsSpec = Joi.object()
--- src/server.js
+++ src/server.js
@@ -1,76 +1,94 @@
-import Hapi from "@hapi/hapi";
+import Inert from "@hapi/inert";
 import Vision from "@hapi/vision";
-import Handlebars from "handlebars";
-import path from "path";
+import Hapi from "@hapi/hapi";
 import Cookie from "@hapi/cookie";
 import dotenv from "dotenv";
+import path from "path";
 import Joi from "joi";
-import Inert from "@hapi/inert";
+import jwt from "hapi-auth-jwt2";
 import HapiSwagger from "hapi-swagger";
-
 import { fileURLToPath } from "url";
+import Handlebars from "handlebars";
+import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+import { validate } from "./api/jwt-utils.js";
 import { apiRoutes } from "./api-routes.js";
-import { webRoutes } from "./web-routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
+
 const swaggerOptions = {
   info: {
     title: "Playtime API",
-    version: "0.1",
+    version: "0.1"
   },
+  securityDefinitions: {
+    jwt: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header"
+    }
+  },
+  security: [{ jwt: [] }]
 };
 
 async function init() {
   const server = Hapi.server({
     port: 3000,
-    host: "localhost",
+    host: "localhost"
   });
 
-  const result = dotenv.config();
-  if(result.error){
-    console.log(result.error.message);
-    process.exit(1);
-  }
-
+  await server.register(Inert);
   await server.register(Vision);
   await server.register(Cookie);
-  await server.register(Inert);
+  await server.register(jwt);
+
   await server.register([
     Inert,
     Vision,
     {
       plugin: HapiSwagger,
-      options: swaggerOptions,
-    },
+      options: swaggerOptions
+    }
   ]);
-  server.validator(Joi);
 
-  server.auth.strategy("session", "cookie", {
-    cookie: {
-      name: process.env.COOKIE_NAME,
-      password: process.env.COOKIE_PASSWORD,
-      isSecure: false,
-    },
-    redirectTo: "/",
-    validateFunc: accountsController.validate,
-  });
-  server.auth.default("session");
+  server.validator(Joi);
 
   server.views({
     engines: {
-      hbs: Handlebars,
+      hbs: Handlebars
     },
     relativeTo: __dirname,
     path: "./views",
     layoutPath: "./views/layouts",
     partialsPath: "./views/partials",
     layout: true,
-    isCached: false,
+    isCached: false
   });
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false
+    },
+    redirectTo: "/",
+    validateFunc: accountsController.validate
+  });
+  server.auth.strategy("jwt", "jwt", {
+    key: process.env.cookie_password,
+    validate: validate,
+    verifyOptions: { algorithms: ["HS256"] }
+  });
+  server.auth.default("session");
+
   db.init("mongo");
   server.route(webRoutes);
   server.route(apiRoutes);
